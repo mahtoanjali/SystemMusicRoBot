d33ca7a8751d4e108c7116dadeeebb775097fefb
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-01 21:31:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/84819fc115cb0eff32b2b.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/aab8ee34e7455ed347c3f.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/aab8ee34e7455ed347c3f.jpg")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/tbcbotschat")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/tbc_bots")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/MAHTOxOFFICIAL")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+fztuVtP1frMwYTk1")
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5090817443").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6866756579").split()))
 
 
 FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
